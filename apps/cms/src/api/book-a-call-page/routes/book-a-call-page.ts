0c26a647d37ba2b1fc672fc9f5ee7a75e1983c5b
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-11-13 22:19:16 +0000

    wip

--- apps/cms/src/api/book-a-call-page/routes/book-a-call-page.ts
+++ apps/cms/src/api/book-a-call-page/routes/book-a-call-page.ts
@@ -2,6 +2,6 @@
  * book-a-call-page router
  */
 
-import { factories } from '@strapi/strapi';
+import {factories} from '@strapi/strapi'
 
-export default factories.createCoreRouter('api::book-a-call-page.book-a-call-page');
+export default factories.createCoreRouter('api::book-a-call-page.book-a-call-page')
