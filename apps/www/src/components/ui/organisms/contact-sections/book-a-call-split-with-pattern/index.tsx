f012b762736991adc6e375e7149ca25d12177ed5
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-11-13 22:19:16 +0000

    wip

--- apps/www/src/components/ui/organisms/contact-sections/book-a-call-split-with-pattern/index.tsx
+++ apps/www/src/components/ui/organisms/contact-sections/book-a-call-split-with-pattern/index.tsx
@@ -1,17 +1,6 @@
-import {BuildingOffice2Icon, CheckCircleIcon} from '@heroicons/react/24/outline'
-import {Checkbox, CheckboxProps} from '@/components/ui/atoms/inputs/checkbox'
-import {Input, InputProps} from '@/components/ui/atoms/inputs/input'
+import {BuildingOffice2Icon} from '@heroicons/react/24/outline'
 import {LocationObject} from '@/models/location.model'
-import {TextArea, TextAreaProps} from '@/components/ui/atoms/inputs/textarea'
-import {contactUsFormData} from '@/models/contact-us-form-data.model'
-import {createHsformsPayload} from '@/utils/toolbox'
-import {sendEmail} from '@/utils/hubspot'
-import {useForm} from 'react-hook-form'
-import {useState} from 'react'
-import Button from '@/components/ui/atoms/button'
-import ConfettiExplosion from 'react-confetti-explosion'
-import Router from 'next/router'
-import useTranslation from 'next-translate/useTranslation'
+import Script from 'next/script'
 
 export type BookACallSplitWithPatternProps = {
   title: string
@@ -23,44 +12,46 @@ export default function BookACallSplitWithPattern(props: BookACallSplitWithPatte
 
   return (<div
     className='relative isolate py-20 lg:py-[104px]'
-    id='contact-section'
   >
-    <div className='mx-auto max-w-7xl'>
-      <div className='grid grid-cols-1 mx-6 lg:grid-cols-2 bg-slate-800 rounded-xl'>
-        <div className='relative px-6 py-8 lg:static lg:pl-12 lg:pr-0 lg:py-14'>
-          <div className='max-w-full mr-auto lg:mx-0 lg:max-w-sm'>
-            <h2 className='text-3xl font-medium text-white sm:text-4xl'>{props.title}</h2>
-            <p className='mt-5 text-[20px] leading-8 text-slate-400 font-light'>
-              {props.subtitle}
-            </p>
-            <dl className='mt-10 space-y-5 text-base leading-[26px] font-light text-slate-400'>
-              {props.locations?.map((location) => (
-                <a
-                  key={location.address}
-                  href={location.gMap}
-                  target='_blank'
-                  className='flex gap-x-4'
-                >
-                  <dt className='flex-none'>
-                    <span className='sr-only'>Address</span>
-                    <BuildingOffice2Icon
-                      className='w-6 h-6 translate-y-1 text-slate-400'
-                      aria-hidden='true'/>
-                  </dt>
-                  <dd>
-                    {location.address}
-                    <br/>
-                    {location.zipCode} {location.city}, {location.country}
-                  </dd>
-                </a>
-              ))}
-            </dl>
-          </div>
+    <div className='pt-32 mx-auto max-w-7xl lg:pt-44'>
+      <div className='flex mx-6 space-x-6 bg-slate-800 rounded-xl'>
+        <div className='relative px-6 py-8 lg:static lg:pl-12 lg:pr-0 lg:py-14 w-[500px]'>
+          <h2 className='text-3xl font-medium text-white sm:text-4xl'>{props.title}</h2>
+          <p className='mt-5 text-[20px] leading-8 text-slate-400 font-light'>
+            {props.subtitle}
+          </p>
+          <dl className='mt-10 space-y-5 text-base leading-[26px] font-light text-slate-400'>
+            {props.locations?.map((location) => (
+              <a
+                key={location.address}
+                href={location.gMap}
+                target='_blank'
+                className='flex gap-x-4'
+              >
+                <dt className='flex-none'>
+                  <span className='sr-only'>Address</span>
+                  <BuildingOffice2Icon
+                    className='w-6 h-6 translate-y-1 text-slate-400'
+                    aria-hidden='true'/>
+                </dt>
+                <dd>
+                  {location.address}
+                  <br/>
+                  {location.zipCode} {location.city}, {location.country}
+                </dd>
+              </a>
+            ))}
+          </dl>        
         </div>
         <div
-          className='px-6 py-8 lg:pl-0 lg:pr-12 lg:py-14'
+          className='flex justify-end flex-1 w-full px-6 py-8 lg:pl-0 lg:pr-10 lg:py-14'
         >
-          
+          <div
+            className='meetings-iframe-container !h-[600px] overflow-hidden -mt-2 w-full'
+            data-src='https://meetings.hubspot.com/56k/team?embed=true'></div>
+          <Script
+            type='text/javascript'
+            src='https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js'></Script>
         </div>
       </div>
     </div>
