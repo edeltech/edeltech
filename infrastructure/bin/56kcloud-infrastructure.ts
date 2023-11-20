c71d9ba28c3a3d67f8e8ab6a46420fd91edbcd6a
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-11-20 22:35:37 +0000

    wip

--- infrastructure/bin/56kcloud-infrastructure.ts
+++ infrastructure/bin/56kcloud-infrastructure.ts
@@ -16,21 +16,25 @@ const acmStack = new AcmStack(app, 'Stack56KcloudAcm', {
   }
 })
 
-new StrapiStack(app, 'Stack56KcloudStrapi', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
-  },
-  acmStack
-})
+if(process.env.DEPLOY_WWW_ONLY !== 'true') {
+  new StrapiStack(app, 'Stack56KcloudStrapi', {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION
+    },
+    acmStack
+  })
+}
 
-new OpenNextStack(app, 'Stack56KcloudOpenNext', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
-  },
-  acmStack
-})
+if(process.env.DEPLOY_CMS_ONLY !== 'true') {
+  new OpenNextStack(app, 'Stack56KcloudOpenNext', {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION
+    },
+    acmStack
+  })
+}
 
 new GithubRunnerStack(app, 'GithubRunnerStack', {
   env: {
