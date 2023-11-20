1ae41c4a5ce3676b36a88bc6233643059e2d11b4
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-11-20 21:35:38 +0000

    wip

--- apps/cms/src/admin/webpack.config.example.js
+++ apps/cms/src/admin/webpack.config.example.js
@@ -5,27 +5,27 @@ import {
 
 const secret_name = 'Stack56KcloudStrapirds56kcl-aN5gupbAgOVy'
 
-
+// eslint-disable-next-line no-unexpected-multiline
 (async () => {
   const client = new SecretsManagerClient({
-  region: 'us-east-1'
-})
+    region: 'us-east-1'
+  })
   let response
 
-try {
-  response = await client.send(
-    new GetSecretValueCommand({
-      SecretId: secret_name,
-      VersionStage: 'AWSCURRENT' // VersionStage defaults to AWSCURRENT if unspecified
-    })
-  )
-} catch (error) {
-  // For a list of exceptions thrown, see
-  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-  throw error
-}
+  // eslint-disable-next-line no-useless-catch
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: 'AWSCURRENT' // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    )
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error
+  }
 
-const secret = response.SecretString
-console.log(secret)
+  const secret = response.SecretString
+  console.log(secret)
 })()
-
