2bb8522978481e307d3a1ad58e3f8de9acb7bb1f
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2023-11-22 10:03:32 +0000

    wip

--- infrastructures/www/lib/nextjs-stack.ts
+++ infrastructures/www/lib/nextjs-stack.ts
@@ -1,3 +1,4 @@
+import {CertificateValidation} from 'aws-cdk-lib/aws-certificatemanager'
 import {Construct} from 'constructs'
 import {Nextjs} from '@56k.cloud/cdk-construct'
 import {Stack, StackProps} from 'aws-cdk-lib'
@@ -7,9 +8,13 @@ export class NextjsStack extends Stack {
     super(scope, id, props)
 
     new Nextjs(this, 'Nextjs', {
-      nextjsPath: '../../apps/www',  
-      domainName: 'open-next.56k.cloud',
-      domainAlias: 'www.open-next.56k.cloud'
+      nextjsPath: '../../apps/www',
+      customDomain: {
+        domainName: 'www.open-next.56k.cloud',
+        domainAlias: 'open-next.56k.cloud',
+        isExternalDomain: true
+      },
+      certificateValidation: CertificateValidation.fromDns()
     })
 
   }
